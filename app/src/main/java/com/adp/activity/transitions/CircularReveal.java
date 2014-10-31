59fbb4e3beaea4e61c053c3c4ebb943231a2dea6
Alex Lockwood <alockwood@google.com>
2014-10-31 15:02:55 -0400

    refactored and cleaned up code

---
+++ app/src/main/java/com/adp/activity/transitions/CircularReveal.java
@@ -0,0 +1,80 @@
+package com.adp.activity.transitions;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.transition.TransitionValues;
+import android.transition.Visibility;
+import android.util.FloatMath;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+
+public class CircularReveal extends Visibility {
+    private final Rect mStartBounds = new Rect();
+    private final int[] mTmpLoc = new int[2];
+
+    public CircularReveal(View view) {
+       setStartView(view);
+    }
+
+    /**
+     * Used to explicitly set the start of the circular reveal.
+     *
+     * TODO: can this be less hacky?
+     */
+    public void setStartView(View v) {
+        v.getLocationInWindow(mTmpLoc);
+        mStartBounds.set(mTmpLoc[0], mTmpLoc[1], mTmpLoc[0] + v.getWidth(), mTmpLoc[1] + v.getHeight());
+    }
+
+    private float getCenterX(View view) {
+        return mStartBounds.left + mStartBounds.width() / 2 - view.getWidth() / 2;
+    }
+
+    private float getCenterY(View view) {
+        return mStartBounds.top + mStartBounds.height() / 2 - view.getHeight() / 2;
+    }
+
+    @Override
+    public Animator onAppear(ViewGroup sceneRoot, View view, TransitionValues startValues, TransitionValues endValues) {
+        if (endValues == null) {
+            return null;
+        }
+        return createAnimation(view, getCenterX(view), getCenterY(view), view.getTranslationX(), view.getTranslationY(), true);
+    }
+
+    private Animator createAnimation(final View v, float startX, float startY, float endX, float endY, final boolean isExpanding) {
+        v.setTranslationX(startX);
+        v.setTranslationY(startY);
+        if (startX == endX && startY == endY) {
+            return null;
+        }
+
+        Path path = getPathMotion().getPath(startX, startY, endX, endY);
+        Animator pathAnimation = ObjectAnimator.ofFloat(v, View.TRANSLATION_X, View.TRANSLATION_Y, path);
+        Animator circularRevealAnimator = revealAnimator(v, v.getMeasuredWidth(), v.getMeasuredHeight(), isExpanding);
+        circularRevealAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                v.setVisibility(isExpanding ? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        final AnimatorSet anim = new AnimatorSet();
+        anim.playTogether(pathAnimation, circularRevealAnimator);
+        anim.setInterpolator(getInterpolator());
+        return anim;
+    }
+
+    private Animator revealAnimator(View view, int width, int height, boolean isExpanding) {
+        float fullRadius = FloatMath.sqrt(width * width / 4f + height * height / 4f);
+        float endRadius = isExpanding ? fullRadius : 0f;
+        float startRadius = isExpanding ? 0f : fullRadius;
+        Animator animator = ViewAnimationUtils.createCircularReveal(view, width / 2, height / 2, startRadius, endRadius);
+        return new NoPauseAnimatorWrapper(animator);
+    }
+}
---
+++ app/src/main/java/com/adp/activity/transitions/util/ParallaxHeaderView.java
@@ -0,0 +1,32 @@
+package com.adp.activity.transitions.util;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+public class ParallaxHeaderView extends FrameLayout {
+    private final Rect mTmpRect = new Rect();
+    private int mVerticalOffset;
+
+    public ParallaxHeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void dispatchDraw(@NonNull Canvas canvas) {
+//        mTmpRect.set(getLeft(), getTop(), getRight(), getBottom() + mVerticalOffset);
+//        canvas.clipRect(mTmpRect);
+        super.dispatchDraw(canvas);
+    }
+
+    public void setVerticalOffset(float offset) {
+//        setTranslationY(offset);
+//        mVerticalOffset = Math.round(offset);
+//        invalidate();
+        Log.i("ParallaxHeaderView", "mVerticalOffset:" + mVerticalOffset);
+    }
+}
